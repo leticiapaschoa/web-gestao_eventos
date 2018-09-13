6d1c945a9f7d1fc494ee6f6851e0e0e8d00bfbbe
LETICIA PASCHOA DA SILVA <171150523@acad.dvuadmin.net>
2018-09-12 21:20:41 -0300

    Estrutura Inicial

---
+++ ProjetoEventos/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using ProjetoEventos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoEventos.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (Session["Usuario"] != null)
+            {
+                ViewBag.Usuario = Session["Usuario"].ToString();
+            }
+
+            if (Request.HttpMethod == "POST")
+            {
+                String Usuario = Request.Form["usuario"].ToString();
+                String Senha = Request.Form["senha"].ToString();
+
+                Login login = new Login();
+                login.Usuario = Usuario;
+                login.Senha = Senha;
+
+                if (login.Buscar())
+                {
+                    Session["Usuario"] = Usuario;
+                    Response.Redirect("Index");
+                }
+                else
+                {
+                    ViewBag.Mensagem = "Usuário ou senha incorretos";
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult Sair()
+        {
+            Session.Clear();
+            Response.Redirect("Index");
+            return View();
+
+        }
+
+    }
+}
---
+++ ProjetoEventos/Models/Login.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoEventos.Models
+{
+    public class Login
+    {
+        public String Usuario { get; set; }
+
+        public String Senha { get; set; }
+
+        public bool Buscar()
+        {
+            bool Resultado = false;
+            SqlConnection Conexao = new SqlConnection();
+            //Conexao.ConnectionString = ConfigurationManager.ConnectionStrings["SENAI"].ConnectionString;
+
+            //Conexao.Open();
+
+            //SqlCommand Comando = new SqlCommand();
+            //Comando.Connection = Conexao;
+
+            //Comando.CommandText = "SELECT * FROM USUARIO WHERE USUARIO=@Usuario AND SENHA=@Senha";
+            //Comando.Parameters.AddWithValue("@Usuario", this.Usuario);
+            //Comando.Parameters.AddWithValue("@Senha", this.Senha);
+
+            //SqlDataReader In = Comando.ExecuteReader();
+
+            //if (In.Read())
+            //{
+            //    Resultado = true;
+            //}            
+
+            //Conexao.Close();
+
+            if(Usuario.Equals("teste") && Senha.Equals("teste")){
+                Resultado = true;
+            }
+
+            return Resultado;
+        }
+    }
+}
