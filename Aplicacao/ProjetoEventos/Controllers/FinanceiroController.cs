5665f0544985cf34b6a85f4bb1beb0af06f713a4
LETICIA PASCHOA DA SILVA <171150523@acad.dvuadmin.net>
2018-10-31 20:05:01 -0300

    Tela do financeiro

---
+++ Aplicacao/ProjetoEventos/Controllers/FinanceiroController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoEventos.Controllers
+{
+    public class FinanceiroController : Controller
+    {
+        // GET: Financeiro
+        public ActionResult Financeiro()
+        {
+            return View();
+        }
+    }
+}
