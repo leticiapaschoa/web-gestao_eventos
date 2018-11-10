f27295216d14b32688f4e9405c9d2a9ea20d20eb
Leticia <leticia.paschoa.silva@gmail.com>
2018-11-10 10:25:55 -0200

    Gráficos de controle estatistico

---
+++ Aplicacao/ProjetoEventos/Controllers/EstatisticaController.cs
@@ -0,0 +1,86 @@
+﻿using ProjetoEventos.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoEventos.Controllers
+{
+    public class EstatisticaController : Controller
+    {
+        // GET: Financeiro
+        public ActionResult Graficos()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult NovoGraficoEventosMes()
+        {
+            List<object> iDados = new List<object>();
+            //Criando dados de exemplo
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Mês", System.Type.GetType("System.String"));
+            dt.Columns.Add("Quantidade", System.Type.GetType("System.Int32"));
+            DataRow dr = dt.NewRow();
+
+            var estatistica = new EstatisticaQntEventos();
+            List<EstatisticaQntEventos> ListaQuantidade = estatistica.BuscaDados();
+
+            foreach (var item in ListaQuantidade)
+            {
+                dr = dt.NewRow();
+                dr["Mês"] = item.Mes;
+                dr["Quantidade"] = item.Quantidade;
+                dt.Rows.Add(dr);
+
+            }
+            
+            //Percorrendo e extraindo cada DataColumn para List<Object>
+            foreach (DataColumn dc in dt.Columns)
+            {
+                List<object> x = new List<object>();
+                x = (from DataRow drr in dt.Rows select drr[dc.ColumnName]).ToList();
+                iDados.Add(x);
+            }
+            //Dados retornados no formato JSON
+            return Json(iDados, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpPost]
+        public JsonResult NovoGraficoEventosTipo()
+        {
+            List<object> iDados = new List<object>();
+            //Criando dados de exemplo
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Tipo", System.Type.GetType("System.String"));
+            dt.Columns.Add("Quantidade", System.Type.GetType("System.Int32"));
+            DataRow dr = dt.NewRow();
+
+            var estatistica = new EstatisticaQntEventosTipo();
+            List<EstatisticaQntEventosTipo> ListaQuantidade = estatistica.BuscaDados();
+
+            foreach (var item in ListaQuantidade)
+            {
+                dr = dt.NewRow();
+                dr["Tipo"] = item.Tipo;
+                dr["Quantidade"] = item.Quantidade;
+                dt.Rows.Add(dr);
+
+            }
+
+            //Percorrendo e extraindo cada DataColumn para List<Object>
+            foreach (DataColumn dc in dt.Columns)
+            {
+                List<object> x = new List<object>();
+                x = (from DataRow drr in dt.Rows select drr[dc.ColumnName]).ToList();
+                iDados.Add(x);
+            }
+            //Dados retornados no formato JSON
+            return Json(iDados, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Aplicacao/ProjetoEventos/Models/EstatisticaQntEventos.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoEventos.Models
+{
+    public class EstatisticaQntEventos
+    {
+        public String Mes { get; set; }
+        public int Quantidade { get; set; }
+
+        public List<EstatisticaQntEventos> BuscaDados()
+        {
+            var stringConnexao = "Server=tcp:unimetrocamp-project.database.windows.net,1433;Initial Catalog=GestaoEvento;Persist Security Info=False;User ID=leticiaps;Password=leticia_paschoa18;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            var listaDados = new List<EstatisticaQntEventos>();
+
+            try
+            {               
+                var connection = new SqlConnection(stringConnexao);
+                SqlDataReader reader;
+                connection.Open();
+
+                var cmd = new SqlCommand("BUSCA_DADOS_GRAFICO_MES", connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var dado = new EstatisticaQntEventos();
+                    dado.Mes = reader["MES"].ToString();
+                    dado.Quantidade = Convert.ToInt16(reader["QUANTIDADE"]);
+
+                    listaDados.Add(dado);
+                }
+
+                reader.Close();
+                
+            }
+            catch (Exception ex)
+            {
+            }
+            return listaDados;
+        }
+    }
+}
